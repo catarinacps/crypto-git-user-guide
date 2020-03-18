da38562148fb78fe166018e98fb050d2a20952c2
Henrique Silva <hcpsilva@inf.ufrgs.br>
2020-03-17 23:43:27 -0300

    more fixes and also now we have color

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -3,13 +3,19 @@
 # author: @hcpsilva
 
 # crashes if an error occurs
-set -euo pipefail
+set -euf -o pipefail
+
+# color and formatting codes using tput
+BOLD=$(tput bold)
+UNDLN=$(tput smul)
+STOUT=$(tput smso)
+RESET=$(tput sgr0)
 
 function usage() {
     cat <<EOF
   $0 [OPTIONS] <URI>
 
-  WHERE [OPTIONS] can be any of the following, in no particular order:
+  ${BOLD}WHERE${RESET} [OPTIONS] can be any of the following, ${UNDLN}in no particular order${RESET}:
     -h | --help
       shows this message and quits
     -n | --dir-name <PATH>
@@ -18,37 +24,42 @@ function usage() {
       be verbose
     -f | --force
       overwrite any existing ecryptfs private directory
-      PLEASE BACKUP YOUR DATA BEFORE DOING THIS
+      ${UNDLN}PLEASE BACKUP YOUR DATA BEFORE DOING THIS${RESET}
     -d | --dry
-      runs in dry mode and doesn't do anything besides that
+      runs in dry mode (print everything) and doesn't do anything besides that
 
-  WHERE <URI> is the URI of your existing repo, ssh format, e.g.:
-    \"git@server:/srv/git/repo.git\"
+  ${BOLD}WHERE${RESET} <URI> is the URI of your existing repo, ssh format, e.g.:
+    "git@server:/srv/git/repo.git"
 EOF
 }
 
 function select_collaborators() {
-    echo 'Available public keys in your GnuPG keyring:' 1>&2
-    echo 1>&2
+    echo -e "\n-> ${BOLD}Select the desired keys${RESET}" 1>&2
+    echo -e '--> Available public keys in your GnuPG keyring:\n' 1>&2
 
-    local PUB_KEYS=($(gpg -k --keyid-format long | grep '^pub' | awk '{print $2}' | cut -d'/' -f2 | xargs))
+    PUB_KEYS=($(gpg -k --keyid-format long | grep '^pub' | awk '{print $2}' | cut -d'/' -f2 | xargs))
     IFS=$'\n' UID_KEYS=($(gpg -k --keyid-format long | grep '^uid' | cut -d']' -f2- | cut -d' ' -f2-))
 
-    local counter=0
-    for kuid in "${UID_KEYS[@]}"; do
-        echo "Index $counter:" 1>&2
-        echo "  $kuid" 1>&2
+    while [ -z "${CHOSEN_KEYS:-}" ]; do
+        local counter=0
+        for kuid in "${UID_KEYS[@]}"; do
+            echo -e "INDEX ${UNDLN}${counter}${RESET}:\n  $kuid\n" 1>&2
+            (( counter = counter + 1 ))
+        done
+
         echo 1>&2
-        (( counter = counter + 1 ))
-    done
 
-    echo 1>&2
-    echo 1>&2
+        read -p "--> Insert the ${UNDLN}INDEXES${RESET} listed above (separated by spaces): " idxs
 
-    read -p 'Insert the indexes of the keys of the collaborators (separated by spaces): ' idxs
+        local IFS=' '
+        for id in $idxs; do
+            CHOSEN_KEYS="${PUB_KEYS[$id]} ${CHOSEN_KEYS:-}"
+        done
 
-    for id in "$idxs"; do
-        CHOSEN_KEYS="${PUB_KEYS[$id]} ${CHOSEN_KEYS:-}"
+        if [ -z "${CHOSEN_KEYS:-}" ]; then
+            echo -e "\n--> ${BOLD}ERROR${RESET}: no keys were chosen!\n" 1>&2
+            exit 5
+        fi
     done
 
     echo "$CHOSEN_KEYS"
@@ -57,7 +68,7 @@ function select_collaborators() {
 for arg; do
     case $arg in
         -h|--help)
-            echo "USAGE:"
+            echo "${BOLD}USAGE${RESET}:"
             usage
             exit
             ;;
@@ -79,9 +90,9 @@ for arg; do
             shift
             ;;
         -*)
-            echo "ERROR: unknown option '$arg'"
+            echo "${BOLD}ERROR${RESET}: unknown option '$arg'"
             echo
-            echo "USAGE:"
+            echo "${BOLD}USAGE${RESET}:"
             usage
             exit 1
             ;;
@@ -99,9 +110,9 @@ DRY=${DRY:-}
 
 # the URI of your existing repo
 if [ -z "${1:+z}" ]; then
-    echo "ERROR: missing positional argument <URI>"
+    echo "${BOLD}ERROR${RESET}: missing positional argument <URI>"
     echo
-    echo "USAGE:"
+    echo "${BOLD}USAGE${RESET}:"
     usage
     exit 2
 else
@@ -109,53 +120,60 @@ else
 fi
 
 if [ ! -x "$(command -v ecryptfs-mount-private)" ]; then
-    echo "ERROR: the necessary ecryptfs tooling isn't available"
+    echo "${BOLD}ERROR${RESET}: the necessary ecryptfs tooling isn't available"
     echo "  please install 'ecryptfs-utils'"
     exit 3
 fi
 
 if [ ! -x "$(command -v git)" ]; then
-    echo "ERROR: git isn't available"
+    echo "${BOLD}ERROR${RESET}: git isn't available"
     echo "  please install 'git'"
     exit 3
 fi
 
-if [ -d "$HOME/.Private" ] && [ $FORCE = 'false' ] && [ -z ${DRY:+z} ]; then
-    echo "ERROR: You have already configured a private directory with ecryptfs"
+if [ -d "$HOME/.Private" ] && [ $FORCE = 'false' ]; then
+    echo "${BOLD}ERROR${RESET}: You have already configured a private directory with ecryptfs"
     echo "  consider using the '-f' flag to overwrite this directory"
-    echo "  PLEASE BACKUP YOUR DATA BEFORE DOING THIS"
+    echo "  and ${BOLD}PLEASE BACKUP YOUR DATA BEFORE DOING THIS${RESET}"
     exit 4
-elif [ $FORCE = 'true' ] && [ -z ${DRY:+z} ]; then
-    echo "PLEASE CONFIRM YOUR CHOICE"
+elif [ $FORCE = 'true' ]; then
+    echo "-> ${BOLD}CONFIRM YOUR CHOICE${RESET}"
 
-    while [ "$confirmation" != 'y' ] || [ "$confirmation" != 'n' ]; do
-        read -p "Please type y or n: " confirmation
+    while [ "${confirmation:-}" != 'y' ] && [ "${confirmation:-}" != 'n' ]; do
+        read -p "--> Please type y or n: " confirmation
     done
 
     if [ "$confirmation" = 'n' ]; then
-        echo "INFO: canceling operation..."
+        echo -e "\n--> ${BOLD}INFO${BOLD}: operation canceled..."
         exit 0
     fi
 
+    [ $VERBOSE = 'true' ] && echo -e "\n--> Unmounting current directory"
     $DRY ecryptfs-umount-private
     PDIR="$(cat $HOME/.ecryptfs/Private.mnt)"
-    $DRY rm -rf $HOME/.Private $HOME/.ecryptfs $PDIR
+    [ $VERBOSE = 'true' ] && echo -e "\n--> Deleting previous config (you need root to do this)"
+    $DRY sudo rm -rf $HOME/.Private $HOME/.ecryptfs $PDIR
+    [ $VERBOSE = 'true' ] && echo
 fi
 
 # final git directory
 GIT_DIR=$HOME/${GIT_DIR:-$(basename $GIT_URI .git)}
 
 # creating the private encripted folder
+[ $VERBOSE = 'true' ] && echo -e "-> ${BOLD}Setting up the directory with ecryptfs${RESET}\n"
 $DRY ecryptfs-setup-private --nopwcheck --noautomount
 
 # move to a better name
+[ $VERBOSE = 'true' ] && echo -e "\n-> Changing the default name"
 $DRY mv $HOME/Private $GIT_DIR
 $DRY echo $(readlink -f $GIT_DIR) > $HOME/.ecryptfs/Private.mnt
 
 # mount the directory
+[ $VERBOSE = 'true' ] && echo -e "\n-> Mounting the volume"
 $DRY ecryptfs-mount-private
 
 # init and pull the repo
+[ $VERBOSE = 'true' ] && echo -e "\n-> ${BOLD}Initializing the repository${RESET}"
 $DRY cd $GIT_DIR
 $DRY git init
 $DRY git remote add origin gcrypt::$GIT_URI
@@ -168,6 +186,7 @@ if [ -z ${DRY:+z} ]; then
 set -euf
 
 echo "pulling..."
+git fetch --all
 git pull
 
 PARTICIPANTS="$(git config --get remote.origin.gcrypt-participants)"
@@ -180,17 +199,16 @@ fi
 EOF
 fi
 
+KEYS="$(select_collaborators)"
 
-$DRY KEYS="$(select_collaborators)"
+[ $VERBOSE = 'true' ] && echo -e "\n-> ${BOLD}Adding selected participants${RESET}"
+$DRY git config remote.origin.gcrypt-participants "$KEYS"
 
-if [ -z ${DRY:+z} ];then
-    git config remote.origin.gcrypt-participants "$KEYS"
-fi
-
-$DRY git pull origin master --allow-unrelated-histories
+[ $VERBOSE = 'true' ] && echo -e "\n-> Updating the repository"
 $DRY git fetch --all
+$DRY git pull origin master --allow-unrelated-histories
 
 # done
-[ $VERBOSE = 'true' ] && echo "INFO: success!"
+[ $VERBOSE = 'true' ] && echo -e "\n\n${BOLD}INFO${RESET}: success!"
 
 exit
